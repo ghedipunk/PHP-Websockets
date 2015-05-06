201a66350a8e77b2d1ad5582a9c714d508b30ba8
Xaraknid <xaraknid@gmail.com>
2015-05-06 05:10:54 -0400

    Create users.php

---
+++ core/users.php
@@ -0,0 +1,24 @@
+<?php
+
+class WebSocketUser {
+
+  public $socket;
+  public $id;
+  public $watcher;
+  public $headers = NULL;
+  public $handshaked = false;
+
+  public $handlingPartialPacket = false;
+  public $readBuffer = "";
+  public $writeNeeded = false;
+  public $writeBuffer = "";
+
+  public $partialMessage = "";
+  
+  public $hasSentClose = false;
+
+  function __construct($id, $socket) {
+    $this->id = $id;
+    $this->socket = $socket;
+  }
+}
