abf84649ed15d74956f16cfaecd2ac242b51bb4d
Xaraknid <xaraknid@gmail.com>
2015-05-06 05:08:07 -0400

    Create libev.php

---
+++ eventloop/libev.php
@@ -0,0 +1,63 @@
+<?php 
+
+//need Ev library http://php.net/manual/fr/book.ev.php
+trait eventloop_libev {
+  final public function run() {
+	  $this->mem = memory_get_usage();
+    switch(Ev::backend()) {
+      case Ev::BACKEND_SELECT : $backend="Select()";
+        break;
+      case Ev::BACKEND_POLL   : $backend="Poll";
+        break;
+      case Ev::BACKEND_EPOLL  : $backend="Epoll"; // use by linux before and after kernels 2.6.9
+        break;
+      case Ev::BACKEND_KQUEUE : $backend="Kqueue"; // use by BSD systems
+        break;
+      case Ev::BACKEND_PORT   : $backend="Event Port"; // use by Solaris systems
+        break;     
+    }
+	  $this->stdout("RUNNING with libev method BACKEND : $backend");
+	  $w_listen = new EvIo($this->master, Ev::READ, function ($w) {
+      $client = socket_accept($this->master);
+      if ( $client === FALSE) {
+        $this->stdout("Failed: socket_accept()");
+        continue;
+      }
+      else if ( $client > 0) {
+        socket_set_nonblock($client)               or print("Failed: socket_set_nonblock() id #".$client);
+        $user = $this->connect($client);
+        $this->stdout("Client #$this->nbclient connected. " . $client);
+        $w_read = new EvIo($client, Ev::READ , function ($wr) use ($client,&$user) {
+          $start=$this->getrps();
+            $this->stdout("read callback");
+            $this->cb_read($user);
+          $this->getrps($start,'<< read');
+        });
+        $this->readWatchers[$user->id]=&$w_read;
+        $w_read->start();
+      }		  
+    });
+    Ev::run();
+  }
+
+  protected function addWriteWatchers(&$user,$open) {
+    if ($open) {
+      $w_write = new EvIo($user->socket, Ev::WRITE , function () use (&$user) {
+        $start=$this->getrps();
+          if ($user->writeNeeded) {
+            $this->stdout("write callback");
+            $this->ws_write($user);
+          }
+        $this->getrps($start,'>> write');
+      });
+      $this->writeWatchers[$user->id]=&$w_write;
+      $w_write->start();	  
+    }
+    else {
+      $this->writeWatchers[$user->id]->stop();
+      $this->writewatchers[$user->id]=null;
+      unset($this->writeWatchers[$user->id]);
+    }
+  }
+}
+
