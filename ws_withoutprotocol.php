25bb77469d110633d818ef7ceec7e0f794baa4db
Xaraknid <xaraknid@gmail.com>
2015-05-07 05:04:12 -0400

    Update and rename testwebsock.php to ws_withoutprotocol.php

--- ws_withoutprotocol.php
+++ ws_withoutprotocol.php
@@ -1,22 +1,33 @@
 #!/usr/bin/env php
 <?php
 
-require_once('./websockets.php');
+//a simple autoload 
+// Structure of $class = directory_filename
+function __autoload($class) {
+    $class=str_replace('_',DIRECTORY_SEPARATOR,$class);
+    require_once('./' . $class . '.php');
+}
 
-class echoServer extends WebSocketServer {
+class echoServer extends core_websockets {
+  // eventloop_[socket,libev] are the eventloop handler you can use. It's set to socket by default.
+  // extensions_ none implemented yet
+  // toolbox_[yourcode] 
+  use eventloop_socket; 
+  
   //protected $maxBufferSize = 1048576; //1MB... overkill for an echo server, but potentially plausible for other applications.
   
-  protected function process ($user, $message) {
-    $this->send($user,$message);
+  protected function onmessage (&$user, $message) {
+    $this->broadcast($message,$user);
+    //$this->send($user,$message);
   }
   
-  protected function connected ($user) {
+  protected function onopen ($user) {
     // Do nothing: This is just an echo server, there's no need to track the user.
     // However, if we did care about the users, we would probably have a cookie to
     // parse at this step, would be looking them up in permanent storage, etc.
   }
   
-  protected function closed ($user) {
+  protected function onclose ($user) {
     // Do nothing: This is where cleanup would go, in case the user had any sort of
     // open files or other objects associated with them.  This runs after the socket 
     // has been closed, so there is no need to clean up the socket itself here.
@@ -24,10 +35,16 @@ class echoServer extends WebSocketServer {
 }
 
 $echo = new echoServer("0.0.0.0","9000");
+//
 
+
+$echo->run();
+
+/*
 try {
   $echo->run();
 }
 catch (Exception $e) {
   $echo->stdout($e->getMessage());
 }
+*/
