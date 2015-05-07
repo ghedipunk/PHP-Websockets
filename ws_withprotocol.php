1c124d2fc5447c73bf57d660c61594efe77fc974
Xaraknid <xaraknid@gmail.com>
2015-05-07 05:12:56 -0400

    Create ws_withprotocol.php

---
+++ ws_withprotocol.php
@@ -0,0 +1,85 @@
+#!/usr/bin/env php
+<?php
+
+//a simple autoload 
+// Structure of $class = directory_filename
+function __autoload($class) {
+    $class=str_replace('_',DIRECTORY_SEPARATOR,$class);
+    require_once('./' . $class . '.php');
+}
+
+class echoServer extends core_websockets {
+  // eventloop_[socket,libev] are the eventloop handler you can use. It's set to socket by default.
+  // protocol_[echobot,broadcasting,json] 
+  // extensions_ none implemented yet
+  // toolbox_[yourcode] 
+  use eventloop_libev,protocol_broadcasting,protocol_echobot;
+  
+  //protected $maxBufferSize = 1048576; //1MB... overkill for an echo server, but potentially plausible for other applications.
+  
+  protected function onmessage (&$user, $message) {
+    if ($user->headers['protocol']) {
+      switch($user->headers['protocol']) {
+        case 'json': $parsed = $this->json_decode($message); // example of parsing method to be process later
+          break;
+        case 'broadcasting': $this->broadcasting($user,$message); // example of standalone method
+          return;
+        case 'echobot': $this->echobot($user,$message); // another example of standalone method
+          return;
+      }
+      // do some code after parsing method use
+      $this->send($user,$this->json_encode($parsed)); 
+    }
+    else { 
+      // Fallback if client specified none protocol
+      // This part and if ($user->headers['protocol']) is pointless if the server $headerProtocolRequired set TRUE
+      // because the connection will fail anyway.
+      $this->broadcast($message,$user);
+    }
+  }
+  
+  protected function onopen ($user) {
+    // Do nothing: This is just an echo server, there's no need to track the user.
+    // However, if we did care about the users, we would probably have a cookie to
+    // parse at this step, would be looking them up in permanent storage, etc.
+  }
+  
+  protected function onclose ($user) {
+    // Do nothing: This is where cleanup would go, in case the user had any sort of
+    // open files or other objects associated with them.  This runs after the socket 
+    // has been closed, so there is no need to clean up the socket itself here.
+  }
+
+  // Overide the method in this file protect the need to reaply this on core/websockets if we update the file.
+  // Also easier to read
+  protected function checkProtocol($protocols) {
+    // Note : If the client specified subprotocol you MUST choose ONLY ONE  of the list otherwise 
+    // the client will close the connection.  It's FIFO order sent by client.
+    foreach($protocols as $subprotocol ) {
+      switch ($subprotocol) {
+        //the list of protocol the server will support
+        case "json":
+        case "msgpack";
+        case "echobot";
+        case "broadcasting";
+          return $subprotocol;
+      }
+    }
+    return false;
+  }
+}
+
+$echo = new echoServer("0.0.0.0","9000");
+//
+
+
+$echo->run();
+
+/*
+try {
+  $echo->run();
+}
+catch (Exception $e) {
+  $echo->stdout($e->getMessage());
+}
+*/
