8f09be4c42d0bb5483ce7dbb507112a0c11288ba
Xaraknid <xaraknid@gmail.com>
2015-05-08 05:46:08 -0400

    Update ws_withprotocol.php

--- ws_withprotocol.php
+++ ws_withprotocol.php
@@ -13,8 +13,24 @@ class echoServer extends core_websockets {
   // protocol_[echobot,broadcasting,json] 
   // extensions_ none implemented yet
   // toolbox_[yourcode] 
-  use eventloop_libev,protocol_broadcasting,protocol_echobot;
+  use eventloop_socket,protocol_broadcasting,protocol_echobot;
   
+  // Configuration Start ( default value uncomment if you need different value ) 
+  //protected $debug_mode							              = false; // debug tool I left in code. verbose mode
+  //protected $max_request_handshake_size           = 1024; // chrome : ~503B firefox : ~530B IE 11 : ~297B 
+  // There is no way to use http status code to send some application error to client we MUST open the connection first
+  //protected $max_client                           = 100;  // 1024 is the max with select() keep space for rejecting socket I suggest keeping 24
+  //protected $error_maxclient   = "WS SERVER reach it maximum limit. Please try again later"; // Set the error message sent to client. 
+  //protected $headerOriginRequired                 = false;
+  //protected $headerProtocolRequired               = false;
+  //protected $willSupportExtensions                = false;  // Turn it to true if you support any extensions
+
+  // TODO : these 2 variables will be used to protect OOM and dynamically set max_client based on mem allowed per user
+  //protected $max_writeBuffer					            = 49152; //48K out 
+  //protected $max_readBuffer					              = 49152; //48K in 
+  // Configuration End
+  
+
   //protected $maxBufferSize = 1048576; //1MB... overkill for an echo server, but potentially plausible for other applications.
   
   protected function onmessage (&$user, $message) {
@@ -28,7 +44,7 @@ class echoServer extends core_websockets {
           return;
       }
       // do some code after parsing method use
-      $this->send($user,$this->json_encode($parsed)); 
+      $this->send($user,$message); 
     }
     else { 
       // Fallback if client specified none protocol
@@ -69,7 +85,7 @@ class echoServer extends core_websockets {
   }
 }
 
-$echo = new echoServer("0.0.0.0","9000");
+$echo = new echoServer("0.0.0.0","8080");
 //
 
 
