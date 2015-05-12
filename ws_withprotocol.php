ae565a6d9b96986a37794be45f884bd992268e39
Xaraknid <xaraknid@gmail.com>
2015-05-12 02:51:45 -0400

    Update ws_withprotocol.php

--- ws_withprotocol.php
+++ ws_withprotocol.php
@@ -1,22 +1,25 @@
 #!/usr/bin/env php
 <?php
+// Same purpose of '@' doing it once
+// Dev environement you should use E_ALL ( unless you like having headache when debugging)
+error_reporting(E_ERROR);  // only show error that will break the server. should be enought for Production
 
 //a simple autoload 
 // Structure of $class = directory_filename
 function __autoload($class) {
-    $class=str_replace('_',DIRECTORY_SEPARATOR,$class);
-    require_once('./' . $class . '.php');
+  $class=str_replace('_',DIRECTORY_SEPARATOR,$class);
+  require_once('./' . $class . '.php');
 }
 
 class echoServer extends core_websockets {
   // eventloop_[socket,libev] are the eventloop handler you can use. It's set to socket by default.
-  // protocol_[echobot,broadcasting,json] 
+  // protocol_[echobot,broadcasting] json and msgpack are there for example ( you can use protocol folder or use it directly here ) 
   // extensions_ none implemented yet
   // toolbox_[yourcode] 
   use eventloop_socket,protocol_broadcasting,protocol_echobot;
   
   // Configuration Start ( default value uncomment if you need different value ) 
-  //protected $debug_mode							              = false; // debug tool I left in code. verbose mode
+  //protected $debug_mode							= false; // debug tool I left in code. verbose mode
   //protected $max_request_handshake_size           = 1024; // chrome : ~503B firefox : ~530B IE 11 : ~297B 
   // There is no way to use http status code to send some application error to client we MUST open the connection first
   //protected $max_client                           = 100;  // 1024 is the max with select() keep space for rejecting socket I suggest keeping 24
@@ -26,8 +29,8 @@ class echoServer extends core_websockets {
   //protected $willSupportExtensions                = false;  // Turn it to true if you support any extensions
 
   // TODO : these 2 variables will be used to protect OOM and dynamically set max_client based on mem allowed per user
-  //protected $max_writeBuffer					            = 49152; //48K out 
-  //protected $max_readBuffer					              = 49152; //48K in 
+  //protected $max_writeBuffer					    = 49152; //48K out 
+  //protected $max_readBuffer					    = 49152; //48K in 
   // Configuration End
   
 
@@ -36,7 +39,9 @@ class echoServer extends core_websockets {
   protected function onmessage (&$user, $message) {
     if ($user->headers['protocol']) {
       switch($user->headers['protocol']) {
-        case 'json': $parsed = $this->json_decode($message); // example of parsing method to be process later
+        case 'json': //$parsed = json_decode($message); // example of parsing method to be process later
+          break;
+        case 'msgpack': //$parsed = msgpack_unpack($message); // example of parsing method to be process later
           break;
         case 'broadcasting': $this->broadcasting($user,$message); // example of standalone method
           return;
@@ -54,10 +59,11 @@ class echoServer extends core_websockets {
     }
   }
   
-  protected function onopen ($user) {
+  protected function onopen (&$user) {
     // Do nothing: This is just an echo server, there's no need to track the user.
     // However, if we did care about the users, we would probably have a cookie to
     // parse at this step, would be looking them up in permanent storage, etc.
+    // Also you can use this for sending a welcome message to the newly client.
   }
   
   protected function onclose ($user) {
@@ -85,17 +91,6 @@ class echoServer extends core_websockets {
   }
 }
 
-$echo = new echoServer("0.0.0.0","9000");
-//
-
+$echo = new echoServer("0.0.0.0","9000"); 
 
 $echo->run();
-
-/*
-try {
-  $echo->run();
-}
-catch (Exception $e) {
-  $echo->stdout($e->getMessage());
-}
-*/
