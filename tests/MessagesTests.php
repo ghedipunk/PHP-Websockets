e9df05f9b654f532b0ca868ccda58055195adf0b
Adam Alexander <ghedipunk@gmail.com>
2015-10-03 22:48:50 -0700

    Includes for tests

--- tests/MessagesTests.php
+++ tests/MessagesTests.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once('../core/interfaces/message.php');
+require_once('../core/interfaces/websocketuser.php');
+require_once('../core/message/message.php');
+require_once('../core/user/users.php');
+
 class MessagesTest extends PHPUnit_Framework_TestCase
 {
 	public function testAddUsers()
